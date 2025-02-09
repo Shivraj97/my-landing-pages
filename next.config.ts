cae1746688d6973b0fe69ee9adcef23a09005dd6
Shivraj97 <shivrajnag@gmail.com>
2025-02-10 01:40:12 +0530

    dummy commit

--- next.config.ts
+++ next.config.ts
@@ -4,17 +4,27 @@ const nextConfig: NextConfig = {
   async rewrites() {
     return [
       {
-        source: "/lp/landing-page-1",
-        destination:
-          "https://learn-nextjs-dashboard-ashy.vercel.app/lp/landing-page-1", // Points to /app/landing-page-1/page.js
-      },
-      {
-        source: "/lp/landing-page-2",
-        destination:
-          "https://learn-nextjs-dashboard-ashy.vercel.app/lp/landing-page-2", // Points to /app/landing-page-2/page.js
+        source: "/lp/:path*",
+        destination: "https://learn-nextjs-dashboard-ashy.vercel.app/lp/:path*", // Points to /app/landing-page-1/page.js
       },
     ];
   },
+  // async redirects() {
+  //   return [
+  //     {
+  //       source: "/lp/landing-page-1",
+  //       destination:
+  //         "https://learn-nextjs-dashboard-ashy.vercel.app/lp/landing-page-1",
+  //       permanent: true,
+  //     },
+  //     {
+  //       source: "/lp/landing-page-2",
+  //       destination:
+  //         "https://learn-nextjs-dashboard-ashy.vercel.app/lp/landing-page-2",
+  //       permanent: true,
+  //     },
+  //   ];
+  // },
 };
 
 export default nextConfig;
